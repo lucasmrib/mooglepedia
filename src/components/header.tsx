ab7f98a3c2aa6d417e37fc1b94632d4ec2d0ac67
Lucas Ribeiro <marquesr@unicamp.br>
2024-10-24 15:36:49 -0300

    ajuste

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ export function Header(){
 	return(
 		<header className="font-inter flex items-center justify-between px-32 py-12 bg-gray-100 text-black">
 			<Link href="/" className="flex row items-center gap-4">
-				<Image src="/logo.png" width={48} height={75} alt="Logo" priority />
+				<Image src="/img/logo.png" width={48} height={75} alt="Logo" priority />
 				<div>
 					<span className="text-3xl font-bold cursor-pointer select-none">MooglePedia</span>
 					<p>A wikipedia for Final Fantasy IX players</p>
