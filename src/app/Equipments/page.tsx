4997fcbd52a3b9c60fde78a2c1ca762a94269b19
Lucas Ribeiro <marquesr@unicamp.br>
2024-10-24 15:08:51 -0300

    ajuste

--- src/app/Equipments/page.tsx
+++ src/app/Equipments/page.tsx
@@ -3,7 +3,7 @@
 import useSWR, { Fetcher } from 'swr'
 import Image from 'next/image'
 
-const fetcher = (...args) => fetch([...args]).then((res) => res.json())
+const fetcher = (...args: any) => fetch([...args]).then((res) => res.json())
 
 export default function Equipments() {
 
