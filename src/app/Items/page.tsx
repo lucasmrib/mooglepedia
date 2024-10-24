8210a0e1c295eb3b4e14e7897c9c19d0b30b737f
Lucas Ribeiro <marquesr@unicamp.br>
2024-10-24 15:21:39 -0300

    ajuste

--- src/app/Items/page.tsx
+++ src/app/Items/page.tsx
@@ -3,7 +3,8 @@
 import useSWR, { Fetcher } from 'swr'
 import Image from 'next/image'
 
-const fetcher: Fetcher<any> = (...args) => fetch([...args]).then((res) => res.json())
+// const fetcher: Fetcher<any> = (...args) => fetch([...args]).then((res) => res.json())
+const fetcher = (url: any) => fetch(url).then((res) => res.json())
 
 export default function Items() {
 
@@ -14,7 +15,7 @@ export default function Items() {
 
   return (
     <div className="mx-14 mb-16 w-full h-fit text-white space-y-8">
-      {data.items.map((item) => (
+      {data.items.map((item: any) => (
         <div key={item.id} className="rounded-md border-4 border-gray-300 space-y-4 bg-[#5e605f]">
           <header className="font-pixel inline-flex items-center gap-4 w-full pl-8 pr-8 p-4 bg-[#505251] text-sm">
             <Image
